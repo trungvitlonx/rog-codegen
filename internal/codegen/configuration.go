a69528d658adcfbec8b156e3d948547ce762398a
Trung Le <trunglq3007@gmail.com>
2024-06-25 23:05:05 +0700

    feat: add configuration

--- internal/codegen/configuration.go
+++ internal/codegen/configuration.go
@@ -1,1 +1,16 @@
 package codegen
+
+type Configuration struct {
+	WorkingDirectory string        `yaml:"working-directory"`
+	PackageName      string        `yaml:"package"`
+	OutputOptions    OutputOptions `yaml:"out-options,omitempty"`
+}
+
+type OutputOptions struct {
+	UserTemplates         map[string]string `yaml:"user-templates,omitempty"`
+	ControllerPrefix      string            `yaml:"controller-prefix,omitempty"`
+	ServicePrefix         string            `yaml:"service-prefix,omitempty"`
+	ControllerParentClass string            `yaml:"controller-parent-class,omitempty"`
+	ServiceParentClass    string            `yaml:"service-parent-class,omitempty"`
+	RegenerateService     bool              `yaml:"regenerate-service"`
+}
